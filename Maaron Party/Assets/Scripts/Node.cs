d9899cb72610f579e1eb1696b94ebbc11b78d2a3
Aaron Wee <azurakeni@gmail.com>
2024-08-03 13:18:41 +0800

    Path direction detection

--- Maaron Party/Assets/Scripts/Node.cs
+++ Maaron Party/Assets/Scripts/Node.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class Node : MonoBehaviour
 {
 	public List<Node> nextNodes;
+	private Vector3 offset = new Vector3(0,0.25f);
+
+	private void OnDrawGizmosSelected() 
+	{
+		Gizmos.color = Color.magenta;
+		foreach (Node node in nextNodes)
+			Gizmos.DrawLine(transform.position + offset, node.transform.position + offset);
+	}
 }
