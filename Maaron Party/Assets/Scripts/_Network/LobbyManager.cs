94b17a36b3ef095c78d915517ad9c02e04cf8156
Aaron Wee <azurakeni@gmail.com>
2024-08-07 17:07:54 +0800

    Added Lobby Ui Player Selection

--- Maaron Party/Assets/Scripts/_Network/LobbyManager.cs
+++ Maaron Party/Assets/Scripts/_Network/LobbyManager.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
 
 public class LobbyManager : MonoBehaviour
 {
 	public static LobbyManager Instance;
 	public Transform spawnHolder;
+	[SerializeField] private GameObject buttons;
+	[SerializeField] private Button hostBtn;
+	[SerializeField] private Button clientBtn;
+
 
 	private void Awake() 
 	{
@@ -13,5 +19,22 @@ public class LobbyManager : MonoBehaviour
 			Instance = this;
 		else
 			Destroy(gameObject);
+		hostBtn.onClick.AddListener(() => {
+			StartHost();
+		});	
+		clientBtn.onClick.AddListener(() => {
+			StartClient();
+		});	
+	}
+
+	public void StartHost()
+	{
+		NetworkManager.Singleton.StartHost();
+		if (buttons != null) buttons.SetActive(false);
+	}
+	public void StartClient()
+	{
+		NetworkManager.Singleton.StartClient();
+		if (buttons != null) buttons.SetActive(false);
 	}
 }
--- Maaron Party/Assets/Scripts/_Network/LobbyObject.cs
+++ Maaron Party/Assets/Scripts/_Network/LobbyObject.cs
@@ -1,12 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
+using TMPro;
 
-public class LobbyObject : MonoBehaviour
+public class LobbyObject : NetworkBehaviour
 {
+	[SerializeField] private GameObject buttons;
+	[SerializeField] private TextMeshProUGUI characterTxt;
+	[SerializeField] private int maxCharacters=4;
+	public NetworkVariable<int> characterInd = new NetworkVariable<int>(
+		0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+
+	public override void OnNetworkSpawn()
+	{
+		characterInd.OnValueChanged += (int prevInd, int newInd) => {
+			ChangeName(newInd);
+		};
+	}
+
+	private void ChangeName(int ind)
+	{
+		gameObject.name = $"__ PLAYER {ind} __";
+		switch (ind)
+		{
+			case 0: 
+				characterTxt.text = "Green";
+				break;
+			case 1: 
+				characterTxt.text = "Orange";
+				break;
+			case 2: 
+				characterTxt.text = "Pink";
+				break;
+			case 3: 
+				characterTxt.text = "Blue";
+				break;
+		}
+	}
+
 	private void Start() 
 	{
 		transform.SetParent(LobbyManager.Instance.spawnHolder, true);
 		transform.localScale = Vector3.one;
+		ChangeName((int) OwnerClientId);
+		if (IsOwner)
+		{
+			buttons.SetActive(true);
+		}
+	}
+
+	public void CHARACTER_IND_INC()
+	{
+		characterInd.Value = (characterInd.Value + 1) % maxCharacters;
+	}
+	public void CHARACTER_IND_DEC()
+	{
+		characterInd.Value = characterInd.Value == 0 ? maxCharacters - 1 : characterInd.Value - 1;
 	}
 }
