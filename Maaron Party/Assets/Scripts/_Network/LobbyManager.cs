4a5b90f913494d5140031d7dc3c55a57653faffc
Aaron Wee <azurakeni@gmail.com>
2024-08-07 17:15:48 +0800

    Implemented Scene Load syncing

--- Maaron Party/Assets/Scripts/_Network/LobbyManager.cs
+++ Maaron Party/Assets/Scripts/_Network/LobbyManager.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
+using UnityEngine.SceneManagement;
 
-public class LobbyManager : MonoBehaviour
+public class LobbyManager : NetworkBehaviour
 {
 	public static LobbyManager Instance;
 	public Transform spawnHolder;
 	[SerializeField] private GameObject buttons;
 	[SerializeField] private Button hostBtn;
 	[SerializeField] private Button clientBtn;
+	[SerializeField] private Button startBtn;
 
 
 	private void Awake() 
@@ -25,16 +27,25 @@ public class LobbyManager : MonoBehaviour
 		clientBtn.onClick.AddListener(() => {
 			StartClient();
 		});	
+		startBtn.onClick.AddListener(() => {
+			StartGame();
+		});	
 	}
 
 	public void StartHost()
 	{
 		NetworkManager.Singleton.StartHost();
 		if (buttons != null) buttons.SetActive(false);
+		if (startBtn != null)
+			startBtn.gameObject.SetActive(true);
 	}
 	public void StartClient()
 	{
 		NetworkManager.Singleton.StartClient();
 		if (buttons != null) buttons.SetActive(false);
+	}
+	public void StartGame()
+	{
+		NetworkManager.Singleton.SceneManager.LoadScene("TestBoard", UnityEngine.SceneManagement.LoadSceneMode.Single);
 	}
 }
